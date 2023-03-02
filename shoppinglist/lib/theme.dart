1312d5d41e75a77494ca1a0a166b0a3f44ebb8fa
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2023-03-02 22:54:41 +0700

    added themes file and linter options

---
+++ shoppinglist/lib/theme.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static final Color _lightPrimaryColor = Colors.blueGrey.shade50;
+  static final Color _lightPrimaryVariantColor = Colors.blueGrey.shade800;
+  static final Color _lightOnPrimaryColor = Colors.blueGrey.shade200;
+  static const Color _lightTextColorPrimary = Colors.black;
+  static final Color _appbarColorLight = Colors.blue;
+
+  static final Color _darkPrimaryColor = Colors.blueGrey.shade900;
+  static const Color _darkPrimaryVariantColor = Colors.black;
+  static final Color _darkOnPrimaryColor = Colors.blueGrey.shade300;
+  static const Color _darkTextColorPrimary = Colors.white;
+  static final Color _appbarColorDark = Colors.blueGrey.shade800;
+
+  static const Color _iconColor = Colors.white;
+
+  static const Color _accentColorDark = Color.fromRGBO(74, 217, 217, 1);
+
+  static const TextStyle _lightHeadingText = TextStyle(
+      color: _lightTextColorPrimary,
+      fontFamily: "Rubik",
+      fontSize: 20,
+      fontWeight: FontWeight.bold);
+
+  static const TextStyle _lightBodyText = TextStyle(
+      color: _lightTextColorPrimary,
+      fontFamily: "Rubik",
+      fontStyle: FontStyle.italic,
+      fontWeight: FontWeight.bold,
+      fontSize: 16);
+
+  static const TextTheme _lightTextTheme = TextTheme(
+    headline1: _lightHeadingText,
+    bodyText1: _lightBodyText,
+  );
+
+  static final InputDecorationTheme _lightInputDecorationTheme =
+      InputDecorationTheme(
+    floatingLabelStyle: const TextStyle(color: _lightTextColorPrimary),
+    focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(color: Colors.grey.shade800)),
+    border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+  );
+
+  static final TextStyle _darkThemeHeadingTextStyle =
+      _lightHeadingText.copyWith(color: _darkTextColorPrimary);
+
+  static final TextStyle _darkThemeBodyTextStyle =
+      _lightBodyText.copyWith(color: _darkTextColorPrimary);
+
+  static final TextTheme _darkTextTheme = TextTheme(
+    headline1: _darkThemeHeadingTextStyle,
+    bodyText1: _darkThemeBodyTextStyle,
+  );
+
+  static final InputDecorationTheme _darkThemeInputDecorationTheme =
+      _lightInputDecorationTheme.copyWith(
+    floatingLabelStyle: const TextStyle(color: _darkTextColorPrimary),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: Colors.grey.shade400),
+    ),
+  );
+
+  static final ThemeData lightTheme = ThemeData(
+      inputDecorationTheme: _lightInputDecorationTheme,
+      scaffoldBackgroundColor: _lightPrimaryColor,
+      appBarTheme: AppBarTheme(
+          color: _appbarColorLight,
+          iconTheme: const IconThemeData(color: _iconColor)),
+      bottomAppBarColor: _appbarColorLight,
+      colorScheme: ColorScheme.light(
+          primary: _lightPrimaryColor,
+          onPrimary: _lightOnPrimaryColor,
+          secondary: _accentColorDark,
+          primaryVariant: _lightPrimaryVariantColor),
+      textTheme: _lightTextTheme);
+
+  static final ThemeData darkTheme = ThemeData(
+      inputDecorationTheme: _darkThemeInputDecorationTheme,
+      scaffoldBackgroundColor: _darkPrimaryColor,
+      appBarTheme: AppBarTheme(
+          color: _appbarColorDark,
+          iconTheme: const IconThemeData(color: _iconColor)),
+      bottomAppBarColor: _appbarColorDark,
+      colorScheme: ColorScheme.dark(
+        primary: _darkPrimaryColor,
+        secondary: _accentColorDark,
+        onPrimary: _darkOnPrimaryColor,
+        primaryVariant: _darkPrimaryVariantColor,
+      ),
+      textTheme: _darkTextTheme);
+}
