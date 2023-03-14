843227e92d00a5d9607dd07b1836a5bd3537f5ee
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2023-03-14 19:38:51 +0700

    image mapper
    map with light and dark icons for lists

---
+++ shoppinglist/lib/core/mapper/image_mapper.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+
+class ImageMapper {
+  final Map<String, Map<String, Icon>> string2iconTest = {
+    "square_list": {
+      "light": const Icon(CupertinoIcons.square_list),
+      "dark": const Icon(CupertinoIcons.square_list_fill)
+    },
+    "airplane": {
+      "light": const Icon(CupertinoIcons.airplane),
+      "dark": const Icon(CupertinoIcons.airplane)
+    },
+    "bag": {
+      "light": const Icon(CupertinoIcons.bag),
+      "dark": const Icon(CupertinoIcons.bag_fill)
+    },
+    "bandage": {
+      "light": const Icon(CupertinoIcons.bandage),
+      "dark": const Icon(CupertinoIcons.bandage_fill)
+    },
+    "bitcoin_circle": {
+      "light": const Icon(CupertinoIcons.bitcoin_circle),
+      "dark": const Icon(CupertinoIcons.bitcoin_circle_fill)
+    },
+    "book": {
+      "light": const Icon(CupertinoIcons.book),
+      "dark": const Icon(CupertinoIcons.book_fill)
+    },
+    "camera": {
+      "light": const Icon(CupertinoIcons.camera),
+      "dark": const Icon(CupertinoIcons.camera_fill)
+    },
+    "cart": {
+      "light": const Icon(CupertinoIcons.cart),
+      "dark": const Icon(CupertinoIcons.cart_fill)
+    },
+    "car": {
+      "light": const Icon(CupertinoIcons.car),
+      "dark": const Icon(CupertinoIcons.car_fill)
+    },
+    "cube_box": {
+      "light": const Icon(CupertinoIcons.cube_box),
+      "dark": const Icon(CupertinoIcons.cube_box_fill)
+    },
+    "laptop": {
+      "light": const Icon(CupertinoIcons.device_laptop),
+      "dark": const Icon(CupertinoIcons.device_laptop)
+    },
+    "document": {
+      "light": const Icon(CupertinoIcons.doc),
+      "dark": const Icon(CupertinoIcons.doc_fill)
+    },
+    "music": {
+      "light": const Icon(CupertinoIcons.double_music_note),
+      "dark": const Icon(CupertinoIcons.double_music_note)
+    },
+    "film": {
+      "light": const Icon(CupertinoIcons.film),
+      "dark": const Icon(CupertinoIcons.film_fill)
+    },
+    "gamecontroller": {
+      "light": const Icon(CupertinoIcons.gamecontroller),
+      "dark": const Icon(CupertinoIcons.gamecontroller_fill)
+    },
+    "gift": {
+      "light": const Icon(CupertinoIcons.gift),
+      "dark": const Icon(CupertinoIcons.gift_fill)
+    },
+    "group": {
+      "light": const Icon(CupertinoIcons.group),
+      "dark": const Icon(CupertinoIcons.group_solid)
+    },
+    "hammer": {
+      "light": const Icon(CupertinoIcons.hammer),
+      "dark": const Icon(CupertinoIcons.hammer_fill)
+    },
+    "house": {
+      "light": const Icon(CupertinoIcons.house),
+      "dark": const Icon(CupertinoIcons.house_fill)
+    },
+    "lab": {
+      "light": const Icon(CupertinoIcons.lab_flask),
+      "dark": const Icon(CupertinoIcons.lab_flask_solid)
+    },
+    "paw": {
+      "light": const Icon(CupertinoIcons.paw),
+      "dark": const Icon(CupertinoIcons.paw_solid)
+    },
+    "sport": {
+      "light": const Icon(CupertinoIcons.sportscourt),
+      "dark": const Icon(CupertinoIcons.sportscourt_fill)
+    },
+    "stopwatch": {
+      "light": const Icon(CupertinoIcons.stopwatch),
+      "dark": const Icon(CupertinoIcons.stopwatch_fill)
+    },
+    "tickets": {
+      "light": const Icon(CupertinoIcons.ticket),
+      "dark": const Icon(CupertinoIcons.tickets_fill)
+    },
+  };
+}
