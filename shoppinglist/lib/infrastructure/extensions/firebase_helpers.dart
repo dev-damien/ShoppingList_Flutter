2d819ff08545c009e00573340a1dad836bef3ca8
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2023-03-14 14:05:48 +0700

    added firebase helpers
    - helper to get the document of the logged in user
    - helper to get his list preview documents
    - added corresponding error

---
+++ shoppinglist/lib/infrastructure/extensions/firebase_helpers.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shoppinglist/core/errors/errors.dart';
+import 'package:shoppinglist/domain/repositories/auth_repository.dart';
+import 'package:shoppinglist/injection.dart';
+
+extension FirestoreExt on FirebaseFirestore {
+  Future<DocumentReference> userDocument() async {
+    final userOption = sl<AuthRepository>().getSignedInUser();
+    final user = userOption.getOrElse(() => throw NotAuthenticatedError());
+
+    return FirebaseFirestore.instance.collection("users").doc(user.id.value);
+  }
+}
+
+extension DocumentReferenceExt on DocumentReference {
+  CollectionReference<Map<String, dynamic>> get ListPreviewCollection =>
+      collection("lists_preview");
+}
+
+//TODO: maybe add extension for the lists as well
