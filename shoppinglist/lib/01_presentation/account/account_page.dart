32ddd1dea85723cdb7089593b2cd17cd34fe377f
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2023-07-27 11:28:22 +0200

    consistent background color for account and settings page

--- shoppinglist/lib/01_presentation/account/account_page.dart
+++ shoppinglist/lib/01_presentation/account/account_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:shoppinglist/01_presentation/account/widgets/account_body.dart';
-import 'package:shoppinglist/01_presentation/settings/widgets/settings_body.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
+
     return CupertinoPageScaffold(
+      backgroundColor:
+          !isDark ? CupertinoColors.secondarySystemBackground : null,
       navigationBar: CupertinoNavigationBar(
         middle: Text('Account'),
       ),
