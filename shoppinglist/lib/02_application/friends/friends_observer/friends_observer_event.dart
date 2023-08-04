b23c2b3e154dcf8c4ccd0e370bfe63af997425d0
Damien Völker <66248616+dev-damien@users.noreply.github.com>
2023-08-04 11:52:06 +0200

    added events and states

--- shoppinglist/lib/02_application/friends/friends_observer/friends_observer_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'friends_observer_bloc.dart';
-
-abstract class FriendsObserverEvent extends Equatable {
-  const FriendsObserverEvent();
-
-  @override
-  List<Object> get props => [];
-}
--- shoppinglist/lib/02_application/friends/observer/friends_observer_bloc.dart
+++ shoppinglist/lib/02_application/friends/observer/friends_observer_bloc.dart
@@ -1,5 +1,8 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shoppinglist/03_domain/entities/friend.dart';
+import 'package:shoppinglist/core/failures/friend_failures.dart';
 
 part 'friends_observer_event.dart';
 part 'friends_observer_state.dart';
